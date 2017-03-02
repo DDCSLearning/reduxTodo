2c736f6a34558b02576f44dbfb1ae600facc1f79
Daniel Deutsch <deudan1010@gmail.com>
2017-03-02 13:01:28 +0100

    add packages, build addtodo component

---
+++ src/AddTodo.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux';
+
+let AddTodo = ({ dispatch }) => {
+  let input
+
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        input.value = ''
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default AddTodo;
