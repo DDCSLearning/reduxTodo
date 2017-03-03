66f3b02ee67fad24dda842bd332fd4d3e26826be
Daniel Deutsch <deudan1010@gmail.com>
2017-03-03 16:34:33 +0100

    update todolist and items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 
-const Todo = ({ onClick, completed, text }) => (
+const Item = ({ onClick, completed, text }) => (
   <li
     onClick={onClick}
     style={{
@@ -11,10 +11,10 @@ const Todo = ({ onClick, completed, text }) => (
   </li>
 )
 
-Todo.propTypes = {
+Item.propTypes = {
   onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 }
 
-export default Todo
+export default Item
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import Item from './Item';
 
-const TodoList = () => (
-  <li>
-    One item
-  </li>
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map(todo => (
+      <Item key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+    ))}
+  </ul>
 );
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  onTodoClick: PropTypes.func.isRequired
+};
 
 export default TodoList;
