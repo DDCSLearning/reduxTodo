059c85e429300029a26d5c322e05eb1bc27f38c3
Daniel Deutsch <deudan1010@gmail.com>
2017-03-03 12:53:22 +0100

    writing other actions and actioncreators

--- src/ducks.js
+++ src/ducks.js
@@ -1,15 +1,30 @@
+
+const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+let nextTodoId = 0;
+
 //actions
 const ADD_TODO = 'ADD_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
 //reducers
 
 //action creators
-let nextTodoId = 0;
-export const addTodo = text => {
+export function addTodo(text) {
   return {
     type: 'ADD_TODO',
     id: nextTodoId++,
     completed: false,
     text
   };
-};
+}
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO, index };
+}
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter };
+}
