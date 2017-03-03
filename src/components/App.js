0f3b722b060f6e2b901dc05bedc78c9ef1d90621
Daniel Deutsch <deudan1010@gmail.com>
2017-03-03 15:11:47 +0100

    restructure and add container folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
 import Filter from './Filter';
